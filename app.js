4121035ecbb58edddf5397bd27ee191cbb7ea9ab
Khalif Ali <khalifaali12@gmail.com>
2022-09-10 15:44:52 -0400

    Build sample routes to send messages

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
+
 const express = require('express');
 const app = express();
 const router = express.Router();
+const twilio = require('twilio');
+const client = new twilio(process.env.TWILIO_ACCNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+
+app.use(express.json());
+app.use(express.urlencoded( { extended: true }));
+
+
 router.get('/', (req,res) => {
   res.send("Hello");
 });
 
-router.get('/sendFanout', (req,res) => {
+router.post('/sendFanout', (req,res) => {
+  console.log(req.body);
+  let recipient = req.body.recipient
+  client.messages
+  .create({
+    body: 'Fanout Alert! Someone you know has been in a car accident!',
+    to: recipient, // Text this number
+    from: process.env.TWILIO_SENDER_NUM, // From a valid Twilio number
+  })
+  .then((message) => console.log(message.sid));
   res.send('SendingFanout');
 });
 
